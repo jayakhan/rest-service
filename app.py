9c0ddf4ab196852607cd1b0dc328220cf7b40724
jayakhan <jayakhan04@gmail.com>
2021-09-08 12:03:06 -0400

    enable github actions

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_research_materials():
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error occurred during calling a request.')
+    logging.exception('An error occurred during a request.')
     return """An internal error occurred: <pre>{}</pre>See logs for full stacktrace.""".format(e), 500
 
 if __name__ == '__main__':
